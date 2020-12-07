6e41ee771fcc928ff0ca8614c270a320e52ce430
chjort <christoffer.hjort1995@gmail.com>
2020-12-07 21:47:04 +0100

    cache data loader

--- write_records.py
+++ write_records.py
@@ -9,10 +9,10 @@ from data.tf_record import tf_serialize_tensor_example
 
 RESIZE_SHAPE = (320, 320)
 
-# IMG_PATH = "/home/crr/datasets/duts/DUTS-TR/DUTS-TR-Image"
-# MASK_PATH = "/home/crr/datasets/duts/DUTS-TR/DUTS-TR-Mask"
-IMG_PATH = "/home/crr/datasets/duts/DUTS-TE/DUTS-TE-Image"
-MASK_PATH = "/home/crr/datasets/duts/DUTS-TE/DUTS-TE-Mask"
+IMG_PATH = "/home/crr/datasets/duts/DUTS-TR/DUTS-TR-Image"
+MASK_PATH = "/home/crr/datasets/duts/DUTS-TR/DUTS-TR-Mask"
+# IMG_PATH = "/home/crr/datasets/duts/DUTS-TE/DUTS-TE-Image"
+# MASK_PATH = "/home/crr/datasets/duts/DUTS-TE/DUTS-TE-Mask"
 
 img_paths = glob.glob(os.path.join(IMG_PATH, "*.*"))
 mask_paths = [os.path.join(MASK_PATH, os.path.splitext(os.path.basename(imgp))[0] + ".png") for imgp in
@@ -26,8 +26,8 @@ td = td.map(tf_serialize_tensor_example)
 td = td.prefetch(-1)
 
 # %%
-# output_record = "/home/crr/datasets/duts/DUTS-TR/train.tfrecord"
-output_record = "/home/crr/datasets/duts/DUTS-TE/test.tfrecord"
+output_record = "/home/crr/datasets/duts/DUTS-TR/train.tfrecord"
+# output_record = "/home/crr/datasets/duts/DUTS-TE/test.tfrecord"
 
 writer = tf.data.experimental.TFRecordWriter(output_record)
 writer.write(td)
